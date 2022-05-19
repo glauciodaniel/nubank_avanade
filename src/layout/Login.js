fbc25101498498abb0be4dc35725fe50ad64dde7
Glaucio Daniel <glauciodaniel@gmail.com>
2022-05-19 14:02:26 -0300

    Deploy

--- src/layout/Login.js
+++ src/layout/Login.js
@@ -1,23 +1,113 @@
-import logo from "../assets/images/logo.svg";
-import "./App.css";
+import { useReducer, useState } from "react";
+import styled from "styled-components";
+import bannerBG from "../assets/images/bannerBG.png";
+
+const Container = styled.div`
+  display: flex;
+  fieldset {
+    width: 60%;
+    border: none;
+  }
+`;
+// mydevice.io
+const Banner = styled.div`
+  width: 50%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: red solid 1px;
+  background: ${(props) =>
+      props.background ? "url(" + props.background + ") #e5e5e5 " : ""}
+    20% 90% no-repeat;
+  @media (max-width: 1278px) {
+    background-size: 60%;
+  }
+`;
+
+const formReducer = (state, event) => {
+  return {
+    ...state,
+    [event.name]: event.value,
+  };
+};
 
 function Login() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Formulário de Login</p>
+  const [formData, setFormData] = useReducer(formReducer, {});
+  const [submitting, setSubmitting] = useState(false);
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const handleChange = (event) => {
+    //Agora estamos usando o reducer.
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  };
+
+  // function expression - Só existe a partir desta linha. Ela não sofre: Hoisting.
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSubmitting(true);
+    setTimeout(() => {
+      setSubmitting(false);
+    }, 3000);
+  };
+
+  return (
+    <Container>
+      <Banner background={bannerBG}>
+        <h1>
+          Complete os campos ao lado para pedir sua Conta e Cartão de crédito
+        </h1>
+      </Banner>
+      <Banner>
+        {submitting && (
+          <div>
+            <p>Enviando o formulário...</p>
+            <ul>
+              {Object.entries(formData).map(([name, value]) => (
+                <li key={name}>
+                  <strong>{name}</strong>: {value.toString()}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+
+        <form onSubmit={handleSubmit}>
+          {/* CPF, Nome completo, Celular, E-mail, Confirmação de e-mail
+           */}
+          <fieldset>
+            <label>
+              CPF:
+              <input
+                type="text"
+                name="cpf"
+                id="cpf"
+                required
+                onChange={handleChange}
+              />
+            </label>
+          </fieldset>
+
+          <fieldset>
+            <label>
+              Senha:
+              <input
+                type="password"
+                name="senha"
+                id="senha"
+                required
+                onChange={handleChange}
+              />
+            </label>
+          </fieldset>
+
+          <button type="submit">Entrar</button>
+        </form>
+      </Banner>
+    </Container>
   );
 }
 
--- src/layout/Register.js
+++ src/layout/Register.js
@@ -1,15 +1,30 @@
 import { useReducer, useState } from "react";
+import styled from "styled-components";
+import bannerBG from "../assets/images/bannerBG.png";
 
-import "./App.css";
-
-// Reducer - Redutor, filtro, um robozinho. É dividido em três partes estado, event, contexto.
-
-//evento - quem dispara o reducer.
-//estado - é a informação armazenada
-//contexto - é a área de atuação.
-
-//criar um reducer para armazenar todos os campos do formulário dinamicamente.
-// reducer isola o estado(a informação digitada) de um componente que criamos.
+const Container = styled.div`
+  display: flex;
+  fieldset {
+    width: 60%;
+    border: none;
+  }
+`;
+// mydevice.io
+const Banner = styled.div`
+  width: 50%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: red solid 1px;
+  background: ${(props) =>
+      props.background ? "url(" + props.background + ") #e5e5e5 " : ""}
+    20% 90% no-repeat;
+  @media (max-width: 1278px) {
+    background-size: 60%;
+  }
+`;
 
 const formReducer = (state, event) => {
   return {
@@ -40,116 +55,121 @@ function Register() {
   };
 
   return (
-    <div className="App">
-      <h1>Formulário de Cadastro</h1>
+    <Container>
+      <Banner background={bannerBG}>
+        <h1>
+          Complete os campos ao lado para pedir sua Conta e Cartão de crédito
+        </h1>
+      </Banner>
+      <Banner>
+        {submitting && (
+          <div>
+            <p>Enviando o formulário...</p>
+            <ul>
+              {Object.entries(formData).map(([name, value]) => (
+                <li key={name}>
+                  <strong>{name}</strong>: {value.toString()}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
 
-      {submitting && (
-        <div>
-          <p>Enviando o formulário...</p>
-          <ul>
-            {Object.entries(formData).map(([name, value]) => (
-              <li key={name}>
-                <strong>{name}</strong>: {value.toString()}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
+        <form onSubmit={handleSubmit}>
+          {/* CPF, Nome completo, Celular, E-mail, Confirmação de e-mail
+           */}
+          <fieldset>
+            <label>
+              CPF:
+              <input
+                type="text"
+                name="cpf"
+                id="cpf"
+                required
+                onChange={handleChange}
+              />
+            </label>
+          </fieldset>
 
-      <form onSubmit={handleSubmit}>
-        {/* CPF, Nome completo, Celular, E-mail, Confirmação de e-mail
-         */}
-        <fieldset>
-          <label>
-            CPF:
-            <input
-              type="text"
-              name="cpf"
-              id="cpf"
-              required
-              onChange={handleChange}
-            />
-          </label>
-        </fieldset>
+          <fieldset>
+            <label>
+              Nome Completo:
+              <input
+                type="text"
+                name="nome"
+                id="nome"
+                required
+                onChange={handleChange}
+              />
+            </label>
+          </fieldset>
 
-        <fieldset>
-          <label>
-            Nome Completo:
-            <input
-              type="text"
-              name="nome"
-              id="nome"
-              required
-              onChange={handleChange}
-            />
-          </label>
-        </fieldset>
+          <fieldset>
+            <label>
+              Celular:
+              <input
+                type="tel"
+                name="celular"
+                id="celular"
+                onChange={handleChange}
+              />
+            </label>
+          </fieldset>
 
-        <fieldset>
-          <label>
-            Celular:
-            <input
-              type="tel"
-              name="celular"
-              id="celular"
-              onChange={handleChange}
-            />
-          </label>
-        </fieldset>
+          <fieldset>
+            <label>
+              E-mail:
+              <input
+                type="email"
+                name="email"
+                id="email"
+                required
+                onChange={handleChange}
+              />
+            </label>
+          </fieldset>
 
-        <fieldset>
-          <label>
-            E-mail:
-            <input
-              type="email"
-              name="email"
-              id="email"
-              required
-              onChange={handleChange}
-            />
-          </label>
-        </fieldset>
+          <fieldset>
+            <label>
+              Confirmação de e-mail:
+              <input
+                type="email"
+                name="confirma_email"
+                id="confirma_email"
+                required
+                onChange={handleChange}
+              />
+            </label>
+          </fieldset>
 
-        <fieldset>
-          <label>
-            Confirmação de e-mail:
-            <input
-              type="email"
-              name="confirma_email"
-              id="confirma_email"
-              required
-              onChange={handleChange}
-            />
-          </label>
-        </fieldset>
+          <fieldset>
+            <label>
+              <input
+                type="checkbox"
+                name="aceito_comunicacao"
+                id="aceito_comunicacao"
+              />
+              Aceito receber comunicações do Nubank por Whatsapp.
+            </label>
+          </fieldset>
 
-        <fieldset>
-          <label>
-            <input
-              type="checkbox"
-              name="aceito_comunicacao"
-              id="aceito_comunicacao"
-            />
-            Aceito receber comunicações do Nubank por Whatsapp.
-          </label>
-        </fieldset>
+          <fieldset>
+            <label>
+              <input
+                type="checkbox"
+                name="aceito_contrato"
+                id="aceito_contrato"
+              />
+              Eu li, estou ciente das condições de tratamento dos meus dados
+              pessoais e dou meu consentimento, quando aplicável, conforme
+              descrito nesta Política de Privacidade.
+            </label>
+          </fieldset>
 
-        <fieldset>
-          <label>
-            <input
-              type="checkbox"
-              name="aceito_contrato"
-              id="aceito_contrato"
-            />
-            Eu li, estou ciente das condições de tratamento dos meus dados
-            pessoais e dou meu consentimento, quando aplicável, conforme
-            descrito nesta Política de Privacidade.
-          </label>
-        </fieldset>
-
-        <button type="submit">Enviar</button>
-      </form>
-    </div>
+          <button type="submit">Enviar</button>
+        </form>
+      </Banner>
+    </Container>
   );
 }
 
